e4b63a9d15035adf0bb75b2437915b68ba5dbf37
SeeleND <70874349+SeeleND@users.noreply.github.com>
2020-09-24 09:57:22 +0800

    Update Cards.h

--- Cards.h
+++ Cards.h
@@ -6,7 +6,7 @@
 #include <QMap>
 #include <QList>
 
-class Hand;
+class CardSeq;
 
 enum CardPoint//卡牌大小用枚举类型表示
 {
@@ -46,37 +46,6 @@ enum CardSuit
     Suit_End
 };
 
-enum HandType
-{
-    Hand_Unknown,			// 未知
-    Hand_Pass,				// 过
-
-    Hand_Single,			// 单
-    Hand_Pair,				// 对
-
-    Hand_Triple,			// 三个
-    Hand_Triple_Single,		// 三带一
-    Hand_Triple_Pair,		// 三带二
-
-    Hand_Plane,				// 飞机，555_666
-    Hand_Plane_Two_Single,	// 飞机带单，555_666_3_4
-    Hand_Plane_Two_Pair,	// 飞机带双，555_666_33_44
-
-    Hand_Seq_Pair,			// 连对，33_44_55(_66...)
-    Hand_Seq_Single,		// 顺子，34567(8...)
-
-    Hand_Bomb,				// 炸弹
-    Hand_Bomb_Single,		// 炸弹带一个
-    Hand_Bomb_Pair,			// 炸弹带一对
-    Hand_Bomb_Two_Single,	// 炸弹带两单
-
-    Hand_Bomb_Jokers,			// 王炸
-    Hand_Bomb_Jokers_Single,	// 王炸带一个
-    Hand_Bomb_Jokers_Pair,		// 王炸带一对
-    Hand_Bomb_Jokers_Two_Single	// 王炸带两单
-
-};
-
 struct Card
 {
     CardPoint point;
@@ -90,7 +59,11 @@ inline bool operator ==(const Card& left, const Card& right)
 
 inline bool operator <(const Card& left, const Card& right)
 {
-    return left.point < right.point;
+    if (left.point == right.point)
+    {
+        return left.suit>right.suit;
+    }
+    else return left.point < right.point;
 }
 
 inline uint qHash(const Card& card)
